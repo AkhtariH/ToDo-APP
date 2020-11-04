898630229f4f6a01eeec8eebe1685972add091c8
Hemran Akhtari <hemran.akhtari@student.nhlstenden.com>
2020-11-04 17:52:12 +0100

    Created Todo model and controller
    created todo model and controller and added migration

---
+++ app/Models/Todo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'value',
+        'image_path',
+        'checked',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'remember_token',
+    ];
+}
